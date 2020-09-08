5b34f90d306d2950f3f67001c558c7f6911eed3e
Pablo Galán Sabugo <70710895+pablogalan1981@users.noreply.github.com>
2020-09-08 10:09:35 -0500

    Update Export-GoogleResources.ps1

--- Export-GoogleResources.ps1
+++ Export-GoogleResources.ps1
@@ -12,11 +12,11 @@
     1. Download and install GAM tool from https://github.com/jay0lee/GAM/releases
 
 .NOTES
-	Author			  Pablo Galan Sabugo <pablogalan1981@gmail.com> 
-	Date		      Nov/2018
+	Author		Pablo Galan Sabugo <pablogalan1981@gmail.com> 
+	Date		Nov/2018
 	Disclaimer: 	This script is provided 'AS IS'. No warrantee is provided either expressed or implied. 
-                BitTitan cannot be held responsible for any misuse of the script.
-  Version: 1.1
+                        BitTitan cannot be held responsible for any misuse of the script.
+        Version: 1.1
 #>
 
 
